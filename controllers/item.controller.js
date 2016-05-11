d4253e013ee95e72ac2a446a1e278b2df0716dad
David Sorrentino <david@weboxstudio.com>
2016-05-11 08:28:31 +0000

    feature(): add controllers

---
+++ controllers/item.controller.js
@@ -0,0 +1,11 @@
+// create a controller for the items
+// attach the following data to it
+
+/*
+ * Ascia bipenne
+ * L’ascia bipenne è una scure a due lame, simbolo del potere minoico.
+ * Spazio occupato: 3 slots
+ * http://khazalidgrungron.altervista.org/axes/05.png
+ */
+
+// attach some behaviours to the controller: read-description, check-if-fits, dispose
