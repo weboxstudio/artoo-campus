cece031cab9f220b81347b6137101969dc4ccc96
Mattia Filippo De Luca <mattia@selfrules.org>
2016-06-08 15:50:57 +0000

    blocco control e services

--- client/subjects/controllers/item.controller.js
+++ client/subjects/controllers/item.controller.js
@@ -1,4 +1,26 @@
 // create a controller for the items
+angular.module('artoo').controller('ItemsCrtl', ($scope) => {
+    console.log('ItemsCrtl runs!');
+   
+    $scope.item = {
+        name : 'Ascia bipenne',
+        description: 'Lascia bipenne è una scure a due lame, simbolo del potere minoico.',
+        slots: 6,
+        image: 'http://khazalidgrungron.altervista.org/axes/05.png',
+    };
+    
+    $scope.checkIfFits = (item, bagSize) => {
+    return item.slots <= bagSize;    
+    };
+    
+    $scope.dispose = () => {
+        $scope.item = undefined;
+    };
+    
+    $scope.toggleDescription = () => {
+        $scope.visibleDescription = !$scope.visibleDescription;
+    };
+});
 // attach the following data to it
 
 /*
---
+++ client/subjects/controllers/player.controller.js
@@ -0,0 +1,39 @@
+angular.module('artoo').controller('PlayerCtrl', ($scope) => {
+   $scope.player = {
+       name: 'Gimli', 
+       hp: 100,
+       exp: 17,
+       level: 2,
+       race: 'dwarf',
+   } 
+   
+   
+   
+   $scope.isDead = () => {
+       return ($scope.player.hp) ? 'No' : 'Yes' ;
+   };
+   //take damage
+   $scope.takeDamage = (damage) => {
+       $scope.player.hp = ($scope.player.hp < damage) ? 0 : $scope.player.hp - damage;
+       
+   };
+   //level up
+   $scope.levelUp = () => {
+       $scope.player.level += 1;
+       $scope.player.exp = 0;
+       $scope.player.hp = 100;
+   }
+   //gain exp
+   $scope.gainExp = (exp) => {
+       var totalExp = $scope.player.exp + exp;
+       
+       if (totalExp >= 100) {
+           $scope.levelUp();
+           totalExp -= 100;
+           
+       }
+       
+       $scope.player.exp = totalExp;
+       
+   };
+});
