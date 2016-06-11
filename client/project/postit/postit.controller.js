dff2ea943ee0a8353691855970ef5fca5fbc1546
David Sorrentino <david@weboxstudio.com>
2016-06-11 09:23:12 +0000

    feature(): add order by to post-its list

--- client/project/postit/postit.controller.js
+++ client/project/postit/postit.controller.js
@@ -1,6 +1,32 @@
 class PostitCtrl {
   constructor($scope, PostitSrv) {
     $scope.PostitSrv = PostitSrv;
+    
+    $scope.orderOptions = [{
+      name: 'title',
+      title: 'Title',
+      reverse: false,
+    }, {
+      name: 'dates.createdAt',
+      title: 'Creation date',
+      reverse: true,
+    }, {
+      name: 'dates.doneAt',
+      title: 'Completion date',
+      reverse: true,
+    }, {
+      name: 'dates.progressAt',
+      title: 'In progress date',
+      reverse: true,
+    }, {
+      name: 'dates.dueAt',
+      title: 'Due date',
+      reverse: true,
+    },];
+    
+    $scope.selectedOrder = $scope.orderOptions[1];
+        
+    $scope.filterOptions = [{},];
   }
 }
 
--- client/project/postit/postit.service.js
+++ client/project/postit/postit.service.js
@@ -2,29 +2,32 @@ class Postit {
   constructor() {
     this.postits = [{
       id: '001',
-      name: 'Buy concert tickets',
+      title: 'Buy concert tickets',
       body: 'Suspendisse potenti. Donec posuere vulputate arcu.',
       status: 'todo',
       dates: {
-        createdAt: new Date(),
+        createdAt: new Date(2016, 5, 12, 16, 55),
+        dueAt: new Date(2016, 6, 12, 16, 55),
       },
     }, {
       id: '002',
-      name: 'Call Marco',
+      title: 'Call Marco',
       body: 'Suspendisse potenti. Donec posuere vulputate arcu.',
       status: 'done',
       dates: {
         createdAt: new Date(2016, 2, 17, 12, 23),
         doneAt: new Date(2016, 2, 17, 19, 23),
+        dueAt: new Date(2016, 3, 12, 19, 23),
         inProgressAt: new Date(2016, 2, 17, 19, 23),
       },
     }, {
       id: '003',
-      name: 'Renew my personal domain',
+      title: 'Renew my personal domain',
       body: 'Suspendisse potenti. Donec posuere vulputate arcu.',
       status: 'progress',
       dates: {
         createdAt: new Date(2016, 5, 16, 12, 23),
+        dueAt: new Date(2016, 6, 20, 12, 23),
         inProgressAt: new Date(2016, 5, 18, 19, 22),
       },
     }];
